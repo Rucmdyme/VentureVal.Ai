0558fd3c203b217e4f6952699e28a58850dcc224
Abhishek Verma <87115561+Rucmdyme@users.noreply.github.com>
2025-09-15 17:06:59 +0530

    main : initial FE commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Typography, Container } from "@mui/material";
+
+function App() {
+  return (
+    <Container maxWidth="sm" sx={{ textAlign: "center", mt: 5 }}>
+      <Typography variant="h2" component="h1" gutterBottom>
+        ventireval.ai
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
