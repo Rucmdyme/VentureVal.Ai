41ee363cdcff50578af48d46db34b3cbaf726bed
Abhishek Verma <87115561+Rucmdyme@users.noreply.github.com>
2025-09-23 18:59:37 +0530

    integration-testing : option chaining added

--- Frontend/src/components/dealSummary/benchmarking/benchmarking.jsx
+++ Frontend/src/components/dealSummary/benchmarking/benchmarking.jsx
@@ -30,7 +30,7 @@ const BenchmarkingTable = ({ benchMarking }) => {
     }));
   };
 
-  const benchmarkingData = mapBenchmarkingData(benchMarking.percentiles);
+  const benchmarkingData = mapBenchmarkingData(benchMarking?.percentiles);
 
   return (
     <Box sx={{ p: 2, border: "1px solid #e0e0e0", borderRadius: 4 }}>
